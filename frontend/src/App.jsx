cd76437d42faa338bee4c32117889d2d58635f0e
smRid <omorsk911@gmail.com>
2025-07-23 23:37:55 +0600

    Add routing and layout components for employee management system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import './App.css'
+import './App.css';
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-
   return (
     <>
-      <ListEmployeeComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          { /* http://localhost:3000 */ }
+          <Route path='/' element = { <ListEmployeeComponent /> }></Route>
+          { /* http://localhost:3000/employees */ }
+          <Route path='/employees' element = { <ListEmployeeComponent /> }></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
